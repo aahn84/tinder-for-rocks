92bdfe2fb3c956413efc8595336619b8b344d845
Angela Ahn <29741357+aahn84@users.noreply.github.com>
2018-03-14 11:53:50 -0700

    hook up AddRock class

--- src/components/RockList.js
+++ src/components/RockList.js
@@ -1,6 +1,29 @@
 import React , { Component } from 'react'
 import Rock from './Rock'
 
+class AddRock extends Component {
+  state = {newRock: ''}
+
+  handleChange = (e) => {
+    this.setState({newRock: e.target.value})
+  }
+
+  handleAddRock = () => {
+    this.props.addRock(this.state.newRock)
+    this.setState({newRock: ''})
+  }
+
+  render() {
+    return (
+      <div>
+        <label> New rock name </label>
+        <input onChange={this.handleChange} />
+        <button onClick={this.handleAddRock}> Add Rock </button>
+      </div>
+    )
+  }
+}
+
 class RockList extends Component {
   state = {
     message: 'My class is no fun and uncreative today :(',
@@ -27,10 +50,33 @@ class RockList extends Component {
     this.setState({message: e.target.value})
   }
 
+  /*
   // alternate method using .bind(this)
   updateMessage2(e) {
     this.setState({message: e.target.value})
   }
+  */
+
+  // handleChange = (e) => {
+  //   this.setState({newRock: e.target.value})
+  // }
+
+  addRock = (rockName) => {
+    let newRock = {
+      name: rockName,
+      type: 'metamorphic',
+      hardness: 'super',
+      age: 'forever young',
+    }
+    this.setState({
+      rocks: [...this.state.rocks, newRock],
+    })
+  }
+
+  // handleAddRock = () => {
+  //   this.props.addRock(this.state.newRock)
+  //   this.setState({newRock: ''})
+  // }
 
   render() {
     let rockElements = this.state.rocks.map((rock, i) => <Rock key={i} {...rock} />)
@@ -39,8 +85,9 @@ class RockList extends Component {
         <h1> Important message! {this.state.message} </h1>
         <h3>List of rocks for you to mingle with</h3>
         <input onChange={this.updateMessage} />
-        <input onChange={this.updateMessage2.bind(this)} />
+        {/* <input onChange={this.updateMessage2.bind(this)} /> */}
         {rockElements}
+        <AddRock addRock={this.addRock} />
       </div>
     )
   }
